e4e03ef698d1b01948280701204901b63b68b29b
bright <bright@gmail.com>
2018-03-26 00:58:38 +0800

    Add mailer support.

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,27 @@
+class OrderMailer < ApplicationMailer
+
+  # [88] update: configure the default from for mail
+  default from: 'Sam Ruby <depot@example.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # [87] update: configure mail deliver mothed, options include :smtp, :sendmail and :test
+  config.action_mailer.delivery_method = :test
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
